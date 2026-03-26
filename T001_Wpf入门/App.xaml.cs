4dce9ed5202432eb0b30f5b932f50f8ed4030451
Tom Tony <TangChenOne@outlook.com>
2026-03-26 09:36:06 +0800

    添加项目文件。

---
+++ T001_Wpf入门/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace T001_Wpf入门
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Console.WriteLine("1.OnStartup(启动应用程序)被触发！");
+        }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+            Console.WriteLine("2.OnActivated(激活应用程序)被触发！");
+        }
+
+        protected override void OnDeactivated(EventArgs e)
+        {
+            base.OnDeactivated(e);
+            Console.WriteLine("3.OnDeactivated(由激活状态变为非激活状态)被触发！");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            Console.WriteLine("4.OnExit(退出应用程序)被触发！");
+        }
+    }
+}
---
+++ T002_Application的生命周期/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace T002_Application的生命周期
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.SourceInitialized += (s, e) => Console.WriteLine("1.MainWindow的SourceInitialized(创建窗体源)被执行");
+
+            this.Activated += (s, e) => Console.WriteLine("2.MainWindow的Activated（设置当前窗体成为前台窗体）被执行");
+
+            this.Loaded += (s, e) => Console.WriteLine("3.MainWindow的Loaded（当前窗体内部所有元素完成布局和呈现）被执行");
+
+            this.ContentRendered += (s, e) => Console.WriteLine("4.MainWindow的ContentRendered（当前窗体的内容呈现之后）被执行");
+
+            this.Deactivated += (s, e) => Console.WriteLine("5.MainWindow的Deactivated（当前窗体成为后台窗体）被执行");
+
+            this.Closing += (s, e) => Console.WriteLine("6.MainWindow的Closing（当前窗体关闭之前）被执行");
+
+            this.Closed += (s, e) => Console.WriteLine("7.MainWindow的Closed（当前窗体关闭之后）被执行");
+
+            this.Unloaded += (s, e) => Console.WriteLine("8.MainWindow的Unloaded（当前窗体从元素树中删除）被执行");
+        }
+    }
+}
