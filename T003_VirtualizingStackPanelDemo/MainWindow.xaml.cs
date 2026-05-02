30aa1c1b29da1956a2788abc7fa702785a15b5c1
Tom Tony <TangChenOne@outlook.com>
2026-05-02 10:09:54 +0800

    20260502数据更新

---
+++ T003_VirtualizingStackPanelDemo/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace T003_VirtualizingStackPanelDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 生成 100000 条测试数据
+            List<string> data = new List<string>();
+            for (int i = 0; i < 100000; i++)
+            {
+                data.Add($"虚拟化列表项 {i}");
+            }
+
+            // 绑定数据
+            listBox.ItemsSource = data;
+        }
+    }
+}
---
+++ T004_VirtualizingStackPanelDemo/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace T004_VirtualizingStackPanelDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 绑定1000条数据
+            List<string> data = new List<string>();
+            for (int i = 0; i < 1000; i++) data.Add($"Item {i}");
+            itemsControl.ItemsSource = data;
+        }
+    }
+}
